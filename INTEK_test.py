d9671ebd036797557b50d20f80d98ef91fd13333
Ngoc Thien Ta <thienta.028@gmail.com>
2019-08-01 14:22:03 +0700

    Fix bugs in the first 11 waypoints

--- INTEK_test.py
+++ INTEK_test.py
@@ -1,14 +1,15 @@
+import math
 def hello(n) :
-    if not isinstance(n, str) :
+    if isinstance(n, str) == False:
         raise ValueError('A string is expected')
     n = n.strip()
     result = "Hello {}!".format(n)
-    return print(result);
+    return (result)
 
 def calculate_hypotenuse(a, b) :
-    import math
+
     result = math.sqrt(a*a + b*b)
-    return print(result);
+    return (result)
 
 def are_all_conditions_true(cond) :
     if cond == [] :
@@ -19,7 +20,7 @@ def are_all_conditions_true(cond) :
             if i == False :
                 result = False
                 break
-    return print(result);
+    return (result)
 
 def is_a_condition_true(cond) :
     if cond == [] :
@@ -30,14 +31,14 @@ def is_a_condition_true(cond) :
             if i == True :
                 result = True
                 break
-    return print(result);
+    return (result)
 
 def filter_integers_greater_than(l, n) :
     lst = []
     for i in l :
         if i > n :
             lst.append(i)
-    return print(lst);
+    return (lst)
 
 def find_cheapest_hotels(hotel_daily_rates,maximum_daily_rate) :
     aff_hotels = []
@@ -51,33 +52,44 @@ def find_cheapest_hotels(hotel_daily_rates,maximum_daily_rate) :
     aff_hotels_names = []
     for i in aff_hotels :
         aff_hotels_names.append(i[0])
-    return print(aff_hotels_names);
+    return (aff_hotels_names)
 
-def calculate_euclidean_distance_between_2_points(p1, p2)
-    import math
+def calculate_euclidean_distance_between_2_points(p1, p2) :
     euclid_dis = math.sqrt(((p1[0]-p2[0])**2) + ((p1[1]-p2[1])**2))
-    return print(euclid_dis);
+    return (euclid_dis)
+
+def calculate_euclidean_distance_between_points(points):
+    if type(points) == type(None) or len(points) < 2 :
+        raise ValueError("The list MUST contain at least 2 points")
+    import math
+    x_coors, y_coors = [], []
+    for i in points :
+        x_coors.append(i[0])
+        y_coors.append(i[1])
+    total_dis = 0
+    for i in range(len(x_coors)-1) :
+        total_dis += math.sqrt((x_coors[i]-x_coors[i+1])**2 + (y_coors[i]-y_coors[i+1])**2)
+    return (total_dis)
 
 def capitalize_words(s):
-    if not isinstance(s, str) :
-        raise TypeError('Not a string')
     if s == None :
-        print(None)
+        return(None)
+    if isinstance(s, str) == False:
+        raise TypeError('Not a string')
     else :
         #remove unnecessary white spaces
         s = s.split()
         news = " ".join(s)
-        #print
-        return print(news.title());
+        #return
+        return (news.title())
 
 def uppercase_lowercase_words(s) :
-    if not isinstance(s, str) :
-        raise TypeError('Not a string')
     if s == None :
-        print(None)
+        return None
+    if isinstance(s, str) == False:
+        raise TypeError('Not a string')
     else :
-        #take out the words        raise TypeError('Not a string')
-
+        #take out the words
         s = s.split()
         news = ""
         #process the words based on indexing and combine them into a new string
@@ -86,14 +98,14 @@ def uppercase_lowercase_words(s) :
                 news += s[i].upper() + " "
             else :
                 news += s[i].lower() + " "
-        return print(news);
+        return (news.strip())
 
 def factorial(n) :
-    if not isinstance(n, int) :
+    if isinstance(n, int) == False :
         raise TypeError('Not an integer')
     if n < 0 :
         raise ValueError('Not a positive integer')
     fact = 1
     for i in range(n) :
             fact *= (i+1)
-    return print(fact);
+    return (fact)
